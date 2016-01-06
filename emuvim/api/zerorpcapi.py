9c252b6c886bc254872fd1dfe3c19fab6fe15570
peusterm <manuel.peuster@uni-paderborn.de>
2016-01-06 16:59:53 +0100

    added basic API registration and connection mechanism

---
+++ emuvim/api/zerorpcapi.py
@@ -0,0 +1,53 @@
+"""
+Distributed Cloud Emulator (dcemulator)
+(c) 2015 by Manuel Peuster <manuel.peuster@upb.de>
+"""
+
+import logging
+import threading
+import zerorpc
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class ZeroRpcApiEndpoint(object):
+
+    def __init__(self, listenip, port):
+        self.dcs = {}
+        self.ip = listenip
+        self.port = port
+        logging.debug("Created API endpoint %s(%s:%d)" % (
+            self.__class__.__name__, self.ip, self.port))
+
+    def connectDatacenter(self, dc):
+        self.dcs[dc.name] = dc
+        logging.info("Connected DC(%s) to API endpoint %s(%s:%d)" % (
+            dc.name, self.__class__.__name__, self.ip, self.port))
+
+    def start(self):
+        thread = threading.Thread(target=self._api_server_thread, args=())
+        thread.daemon = True
+        thread.start()
+        logging.debug("Started API endpoint %s(%s:%d)" % (
+            self.__class__.__name__, self.ip, self.port))
+
+    def _api_server_thread(self):
+        s = zerorpc.Server(MultiDatacenterApi(self.dcs))
+        s.bind("tcp://%s:%d" % (self.ip, self.port))
+        s.run()
+
+
+class MultiDatacenterApi(object):
+
+    def __init__(self, dcs):
+        self.dcs = dcs
+
+    def compute_action_start(self, dc_name, compute_name):
+        # TODO return UUID / IP ?
+        logging.info("compute start")
+
+    def compute_action_stop(self, dc_name, compute_name):
+        logging.info("compute stop")
+
+    def compute_list(self):
+        pass
---
+++ emuvim/cli/__main__.py
@@ -0,0 +1,24 @@
+"""
+ For now only a dummy client. Connects to the zerorpc interface of the
+ emulator and performs some actions (start/stop/list).
+"""
+import time
+import zerorpc
+
+
+def main():
+    print "Example CLI client"
+    # create connection to remote Mininet instance
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+
+    # do some API tests
+    print c.compute_action_start("dc2", "my_new_container1")
+
+    time.sleep(5)
+
+    print c.compute_action_stop("dc2", "my_new_container1")
+
+
+if __name__ == '__main__':
+    main()
