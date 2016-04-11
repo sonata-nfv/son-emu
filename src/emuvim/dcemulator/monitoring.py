ed711fdbdf3da1adc3108b13129e0d1749c24fb7
stevenvanrossem <steven.vanrossem@intec.ugent.be>
2016-04-11 16:59:29 +0200

    updated SDN chaining commands

--- src/emuvim/dcemulator/monitoring.py
+++ src/emuvim/dcemulator/monitoring.py
@@ -21,38 +21,35 @@ class DCNetworkMonitor():
 
         self.previous_measurement = 0
         self.previous_monitor_time = 0
+        self.switch_dpid = 0
+        self.metric_key = None
+        self.mon_port = None
 
-    def get_rate(self, vnf_name, direction='tx', metric='packets'):
+    # first set some parameters, before measurement can start
+    def setup_rate_measurement(self, vnf_name, vnf_interface=None, direction='tx', metric='packets'):
         # check if port is specified (vnf:port)
-        try:
-            vnf_interface = vnf_name.split(':')[1]
-        except:
+        if vnf_interface is None:
             # take first interface by default
             connected_sw = self.net.DCNetwork_graph.neighbors(vnf_name)[0]
             link_dict = self.net.DCNetwork_graph[vnf_name][connected_sw]
             vnf_interface = link_dict[0]['src_port_id']
-            # vnf_source_interface = 0
-
-        vnf_name = vnf_name.split(':')[0]
-        # take into account that this is a MultiGraph
-        #mon_port = self.net.DCNetwork_graph[vnf_name][connected_sw][0]['dst_port']
 
         for connected_sw in self.net.DCNetwork_graph.neighbors(vnf_name):
             link_dict = self.net.DCNetwork_graph[vnf_name][connected_sw]
             for link in link_dict:
-                # logging.info("{0},{1}".format(link_dict[link],vnf_source_interface))
+                # logging.info("{0},{1}".format(link_dict[link],vnf_interface))
                 if link_dict[link]['src_port_id'] == vnf_interface:
                     # found the right link and connected switch
                     # logging.info("{0},{1}".format(link_dict[link]['src_port_id'], vnf_source_interface))
-                    #src_sw = connected_sw
-
-                    mon_port = link_dict[link]['dst_port']
+                    self.mon_port = link_dict[link]['dst_port']
                     break
 
         try:
             # default port direction to monitor
             if direction is None:
                 direction = 'tx'
+            if metric is None:
+                metric = 'packets'
 
             vnf_switch = self.net.DCNetwork_graph.neighbors(str(vnf_name))
 
@@ -66,21 +63,31 @@ class DCNetworkMonitor():
                 vnf_switch = vnf_switch[0]
             next_node = self.net.getNodeByName(vnf_switch)
 
-            if not isinstance( next_node, OVSSwitch ):
+            if not isinstance(next_node, OVSSwitch):
                 logging.info("vnf: {0} is not connected to switch".format(vnf_name))
                 return
 
+            self.previous_measurement = 0
+            self.previous_monitor_time = 0
 
-            switch_dpid = x = int(str(next_node.dpid),16)
+            #self.switch_dpid = x = int(str(next_node.dpid), 16)
+            self.switch_dpid = int(str(next_node.dpid), 16)
+            self.metric_key = '{0}_{1}'.format(direction, metric)
 
-            # TODO get metric name from arg
-            key = '{0}_{1}'.format(direction, metric)
+        except Exception as ex:
+            logging.exception("get_txrate error.")
+            return ex.message
 
 
-            ret = self.REST_cmd('stats/port', switch_dpid)
+    # call this function repeatedly for streaming measurements
+    def get_rate(self, vnf_name, vnf_interface=None, direction='tx', metric='packets'):
+
+            key = self.metric_key
+
+            ret = self.REST_cmd('stats/port', self.switch_dpid)
             port_stat_dict = ast.literal_eval(ret)
-            for port_stat in port_stat_dict[str(switch_dpid)]:
-                if port_stat['port_no'] == mon_port:
+            for port_stat in port_stat_dict[str(self.switch_dpid)]:
+                if port_stat['port_no'] == self.mon_port:
                     port_uptime = port_stat['duration_sec'] + port_stat['duration_nsec'] * 10 ** (-9)
                     this_measurement = port_stat[key]
 
@@ -89,25 +96,18 @@ class DCNetworkMonitor():
                         self.previous_monitor_time = port_uptime
                         # do first measurement
                         time.sleep(1)
-                        byte_rate = self.get_rate(vnf_name,direction)
+                        byte_rate = self.get_rate(vnf_name, vnf_interface, direction, metric)
                         return byte_rate
                     else:
                         time_delta = (port_uptime - self.previous_monitor_time)
                         byte_rate = (this_measurement - self.previous_measurement) / float(time_delta)
                         #logging.info('uptime:{2} delta:{0} rate:{1}'.format(time_delta,byte_rate,port_uptime))
-                        #return byte_rate
 
                     self.previous_measurement = this_measurement
                     self.previous_monitor_time = port_uptime
                     return byte_rate
 
             return ret
-
-        except Exception as ex:
-            logging.exception("get_txrate error.")
-            return ex.message
-
-
 
     def REST_cmd(self, prefix, dpid):
         url = self.REST_api + '/' + str(prefix) + '/' + str(dpid)
