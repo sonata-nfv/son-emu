000237b471e937907309bb767077a95a4eb2af36
stevenvanrossem <steven.vanrossem@intec.ugent.be>
2016-06-28 13:15:28 +0200

    update ci test script

--- utils/ci/test_sdk_monitor.sh
+++ utils/ci/test_sdk_monitor.sh
@@ -14,7 +14,24 @@ son-emu-cli monitor setup_metric -vnf vnf1:output --metric tx_packets
 
 sleep 5
 
-# check if metric is monitored as expected
+# check if metric is monitored as expected (exported by son-emu, has vnf name as metric id)
+tx_rate=$(son-emu-cli monitor prometheus -d datacenter1 -vnf vnf1 -q 'rate(sonemu_tx_count_packets{vnf_name="vnf1"}[1m])')
+
+
+# test if prometheus query worked
+echo $tx_rate
+
+regex='\[[0-9.]*, .*\]'
+
+if [[ $tx_rate =~ $regex ]] ; then
+	echo " tx_rate monitor test OK"
+else
+	echo "tx_rate monitor test not OK"
+	exit 1
+fi
+
+
+# check if cpu load can be monitored (exported by cAdvisor, needs uuid)
 cpu_load=$(son-emu-cli monitor prometheus -d datacenter1 -vnf vnf1 -q 'sum(rate(container_cpu_usage_seconds_total{id="/docker/<uuid>"}[10s]))')
 
 sleep 1
@@ -27,10 +44,11 @@ regex='\[[0-9.]*, .*\]'
 
 if [[ $cpu_load =~ $regex ]] ; then
 	echo " cpu monitor test OK"
-	exit 0
 else
 	echo "cpu monitor test not OK"
 	exit 1
 fi
+
+exit 0
 
 
