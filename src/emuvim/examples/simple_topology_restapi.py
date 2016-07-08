0b9becf75dfc5fb5771fec8ae5f59948cf2f0a40
Manuel Peuster <manuel@peuster.de>
2016-07-08 13:46:32 +0200

    cleanup and license header for examples and tests

--- src/emuvim/examples/simple_topology_restapi.py
+++ src/emuvim/examples/simple_topology_restapi.py
@@ -1,20 +1,29 @@
 """
-This is an example topology for the distributed cloud emulator (dcemulator).
-(c) 2015 by Manuel Peuster <manuel.peuster@upb.de>
+Copyright (c) 2015 SONATA-NFV
+ALL RIGHTS RESERVED.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This is an example that shows how a user of the emulation tool can
-define network topologies with multiple emulated cloud data centers.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-The definition is done with a Python API which looks very similar to the
-Mininet API (in fact it is a wrapper for it).
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-We only specify the topology *between* data centers not within a single
-data center (data center internal setups or placements are not of interest,
-we want to experiment with VNF chains deployed across multiple PoPs).
+Neither the name of the SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
+nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written
+permission.
 
-The original Mininet API has to be completely hidden and not be used by this
-script.
+This work has been performed in the framework of the SONATA project,
+funded by the European Commission under Grant number 671517 through
+the Horizon 2020 and 5G-PPP programmes. The authors would like to
+acknowledge the contributions of their colleagues of the SONATA
+partner consortium (www.sonata-nfv.eu).
 """
 import logging
 from mininet.log import setLogLevel
