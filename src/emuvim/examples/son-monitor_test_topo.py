db2f94382278b94fe9f8bbfaeb505b8657f8f387
stevenvanrossem <steven.vanrossem@intec.ugent.be>
2016-08-20 00:01:11 +0200

    deploy SAP endpoints via flag for dummygatekeeper

--- src/emuvim/examples/son-monitor_test_topo.py
+++ src/emuvim/examples/son-monitor_test_topo.py
@@ -67,7 +67,7 @@ def create_topology1():
     rapi1.start()
 
     # add the SONATA dummy gatekeeper to each DC
-    sdkg1 = SonataDummyGatekeeperEndpoint("0.0.0.0", 5000)
+    sdkg1 = SonataDummyGatekeeperEndpoint("0.0.0.0", 5000, deploy_sap=True)
     sdkg1.connectDatacenter(dc1)
     sdkg1.connectDatacenter(dc2)
     # run the dummy gatekeeper (in another thread, don't block)
